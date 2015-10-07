2638a5dffb04f4d5df78742773df38831419b9b9
weifei365 <weifei365@gmail.com>
2015-10-07 16:32:12 +0800

    完成init接口中的逻辑； 调整__proto__中函数的调用方式，修复this对象访问错误的问题；

--- src/charts/option.js
+++ src/charts/option.js
@@ -1,4 +1,8 @@
 var core = require('../utils/core.js');
+var ERR = require('../utils/errors.js');
+
+var Tooltip = require('../components/tooltip/tooltip.js');
+var Legend = require('../components/legend/legend.js');
 
 var Option = {
 	element: null,
@@ -16,7 +20,7 @@ var Option = {
 //1. 完全覆盖: this.__proto__ = merge(interface.call(this), this.__proto__);
 //
 //2. 混合使用: this.__proto__ = merge(this.__proto__, interface.call(this, true, this.__proto__, false));
-var API_Interface = function (isMerge, proto, isFirst) {
+var API_Interface = function (isMerge, proto, firstCall) {
 	var that = this;
 	var __proto__ = core.cloneDeep(proto) || {};
 
@@ -25,74 +29,107 @@ var API_Interface = function (isMerge, proto, isFirst) {
 			delete this._;
 			return this;
 		},
-		init: function () {
-			isMerge && isFirst && core.isFunction(__proto__.init) && __proto__.init();
+		init: function (tag) {
+			isMerge && firstCall && core.isFunction(__proto__.init) && __proto__.init.call(that);
+
+			var option = that.option;
+			ERR(!core.isElement(option.element), '1003');
+
+			//init storage space
+			var tools = that.tools = {};
+
+			//build chart container
+			!core.isString(tag) && (tag = 'svg');
+			tools.container = d3.select(option.element).append('svg')
+				.classed('d3chart d3chart-container', true);
+			tools.containerG = tools.container.append('g')
+				.classed('svg-container', true);
+			tools.containerE = tools.containerG.append('g')
+				.classed('entity', true);
+
+			//init tooltip component
+			tools.tooltip = new Tooltip(core.merge({
+				element: option.element
+			}, option.tooltip));
+
+			//init legend component
+			tools.legend = new Legend(core.merge({
+				element: tools.containerG[0][0]
+			}, option.legend));
 
 			//TODO
-			console.log('init in interface');
 
-			isMerge && !isFirst && core.isFunction(__proto__.init) && __proto__.init();
+			isMerge && !firstCall && core.isFunction(__proto__.init) && __proto__.init.call(that);
 
 			return that;
 		},
 		update: function () {
-			isMerge && isFirst && core.isFunction(__proto__.update) && __proto__.update();
+			isMerge && firstCall && core.isFunction(__proto__.update) && __proto__.update.call(that);
 
 			//TODO
 			
-			isMerge && !isFirst && core.isFunction(__proto__.update) && __proto__.update();
+			isMerge && !firstCall && core.isFunction(__proto__.update) && __proto__.update.call(that);
 
 			return that;
 		},
 		redraw: function () {
-			isMerge && isFirst && core.isFunction(__proto__.redraw) && __proto__.redraw();
+			isMerge && firstCall && core.isFunction(__proto__.redraw) && __proto__.redraw.call(that);
 
 			//TODO
 			
-			isMerge && !isFirst && core.isFunction(__proto__.redraw) && __proto__.redraw();
+			isMerge && !firstCall && core.isFunction(__proto__.redraw) && __proto__.redraw.call(that);
 
 			return that;
 		},
 		setOption: function () {
-			isMerge && isFirst && core.isFunction(__proto__.setOption) && __proto__.setOption();
+			isMerge && firstCall && core.isFunction(__proto__.setOption) && __proto__.setOption.call(that);
 
 			//TODO
 			
-			isMerge && !isFirst && core.isFunction(__proto__.setOption) && __proto__.setOption();
+			isMerge && !firstCall && core.isFunction(__proto__.setOption) && __proto__.setOption.call(that);
 
 			return that;
 		},
 		setTooltip: function () {
-			isMerge && isFirst && core.isFunction(__proto__.setTooltip) && __proto__.setTooltip();
+			isMerge && firstCall && core.isFunction(__proto__.setTooltip) && __proto__.setTooltip.call(that);
 
 			//TODO
 			
-			isMerge && !isFirst && core.isFunction(__proto__.setTooltip) && __proto__.setTooltip();
+			isMerge && !firstCall && core.isFunction(__proto__.setTooltip) && __proto__.setTooltip.call(that);
+
+			return that;
+		},
+		setLegend: function () {
+			isMerge && firstCall && core.isFunction(__proto__.setLegend) && __proto__.setLegend.call(that);
+
+			//TODO
+			
+			isMerge && !firstCall && core.isFunction(__proto__.setLegend) && __proto__.setLegend.call(that);
 
 			return that;
 		},
 		bindEvents: function () {
-			isMerge && isFirst && core.isFunction(__proto__.bindEvents) && __proto__.bindEvents();
+			isMerge && firstCall && core.isFunction(__proto__.bindEvents) && __proto__.bindEvents.call(that);
 
 			//TODO
 			
-			isMerge && !isFirst && core.isFunction(__proto__.bindEvents) && __proto__.bindEvents();
+			isMerge && !firstCall && core.isFunction(__proto__.bindEvents) && __proto__.bindEvents.call(that);
 
 			return that;
 		},
 		getHTML: function () {
-			isMerge && isFirst && core.isFunction(__proto__.getHTML) && __proto__.getHTML();
+			isMerge && firstCall && core.isFunction(__proto__.getHTML) && __proto__.getHTML.call(that);
 
 			//TODO
 			
-			isMerge && !isFirst && core.isFunction(__proto__.getHTML) && __proto__.getHTML();
+			isMerge && !firstCall && core.isFunction(__proto__.getHTML) && __proto__.getHTML.call(that);
 		},
 		destroy: function () {
-			isMerge && isFirst && core.isFunction(__proto__.destroy) && __proto__.destroy();
+			isMerge && firstCall && core.isFunction(__proto__.destroy) && __proto__.destroy.call(that);
 
 			//TODO
 			
-			isMerge && !isFirst && core.isFunction(__proto__.destroy) && __proto__.destroy();
+			isMerge && !firstCall && core.isFunction(__proto__.destroy) && __proto__.destroy.call(that);
 		}
 	}._();
 };
