d06eb8491f84c1c4f531f32f5640bfde0b934198
weifei <weifei@patsnap.com>
2015-10-23 13:38:56 +0800

    提供tooltip组件导出

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 
 module.exports = {
 	TreeMap: window.TreeMap = require('./charts/treemap/treemap.js'),
+	Tooltip: window.Tooltip = require('./components/tooltip.js')
 	// TreeMapHelper: window.TreeMapHelper = require('./charts/treemap/treemap_helper.js')
 };
